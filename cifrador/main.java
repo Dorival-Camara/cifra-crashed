f2bbe000ff8534ea852f4d80efe02ad0f48a6218
Dorival-Camara <garfixsd@gmail.com>
2026-04-13 19:21:56 -0300

    Iniciando cifrador

--- cifrador/main.java
+++ cifrador/main.java
@@ -3,16 +3,28 @@ import java.util.Scanner;
 public class main{
     public static void main (String[] args){
 
-        Scanner scanner = new Scanner(System.in);
+        String fraseOriginal = "Quero ser programador. Aprendi Java no Devisate Marília.";
 
         System.out.println("---------------------------------");
         System.out.println("     BEM VINDO AO CIFRADOR");        
         System.out.println("---------------------------------");
-        System.out.println("Insira uma frase para cifrá-la: ");
-        String frase = scanner.nextLine();
 
-        int caracteres = frase.length();
-        System.out.println(caracteres);
+        // Aqui é feita a limpeza da frase
+        String fraseLimpa = limparTexto(fraseOriginal);
+        
+        System.out.println("\nFrase original : ");
+        System.out.println(fraseOriginal);
+
+        System.out.println("\nFrase limpa : ");
+        System.out.println(fraseLimpa);
+
+        System.out.println("\nTamanho da frase limpa :  "+fraseLimpa.length());
+
+        // Aqui é onde a frase será cifrada e decifrada com salto de 5 caracteres
+        int salto = 5;
+
+        System.out.println("" +salto);
+
 
     }
 }
